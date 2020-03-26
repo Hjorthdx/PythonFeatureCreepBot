e104c865df636be3a6a9efefe57845c8805d33e9
Søren Hjorth Boelskifte <sboels18@student.aau.dk>
2020-03-26 16:38:55 +0100

    OOP
    Implementing user class and db in seperate document.
    Not finished. needs to correctly call the users add/remove methods

---
+++ User.py
@@ -0,0 +1,36 @@
+import Db
+
+class User:
+
+    def __init__(self, name, intUserID, strUserID):
+        self.name = name
+        self.intUserID = intUserID
+        self.strUserID = strUserID
+
+    def AddOpdut(self):
+        Db.mycol.update_one(
+            { "Name": self.name },
+            { "$inc": {"Opdutter": 1}}
+        )
+
+    def removeOpdut(self):
+        Db.mycol.update_one(
+            { "Name": self.name },
+            { "$inc": {"Opdutter": -1}}
+        )
+
+    def AddNeddut(self):
+        Db.mycol.update_one(
+            { "Name": self.name },
+            { "$inc": {"Neddutter": 1}}
+        )
+
+    def removeNeddut(self):
+        Db.mycol.update_one(
+            { "Name": self.name },
+            { "$inc": {"Neddutter": -1}}
+        )
+
+
+# TODO
+# Needs methods that adds and removes opdutter and neddutter, so the bot.py is a lot nicer looking
